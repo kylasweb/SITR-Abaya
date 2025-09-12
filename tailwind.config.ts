784fc0e0ff9aaa5adcea9ed644a94be14167416d
Aliya Surayya <aliyaasurayya@gmail.com>
2025-09-12 14:50:40 +0000

    Make the following changes:
    Find this element:
    
      <h1 class="font-headl

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,7 +18,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['"Times New Roman"', 'Times', 'serif'],
-        headline: ['"Tan Pearl"', 'Lora', 'serif'],
+        headline: ['"Playfair Display"', 'serif'],
         code: ['monospace'],
       },
       colors: {
