5e1743e6a4d0010e9776681735622e84ed8d8ad0
Kailas Nair <kailaspnair@gmail.com>
2025-09-13 17:20:20 +0000

    <anonymous code>:1:147461 unreachable code after return statement node_m

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
   description: 'Luxury abayas.',
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
